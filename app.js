83eac20f979088270352f71d83ee96904eab2c05
Ryuichi Itoi <t.link.full.contact@gmail.com>
2014-01-15 23:50:38 -0800

    amendments based on mentor input

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$('#trash').droppable({
 		accept: '.items',
 		hoverClass: "ui-state-hover",
-		drop: function(event, ui){
+		drop: function(e, ui){
 			ui.draggable.remove();
 		}		
 	});
@@ -18,11 +18,14 @@ $(document).ready(function(){
 		$(this).prop('check',true);
 	});
 	
-	$('#addToList').submit(function(event){
-		
-		if($('#textBox').val() != ''){
+	$('#addToList').submit(function(e){
+	    e.preventDefault();
+	    
+	    var listName = $('#textBox').val();
+	    
+		if(listName != '' || listName != null){
 			
-			AddListItem();
+			AddListItem(listName,e);
 			
 			$('.items').draggable({
 				revert: true
@@ -32,82 +35,102 @@ $(document).ready(function(){
 			alert("Please enter shopping list item value");
 		}
 		
-		event.preventDefault();
-		
 		$('#textBox').val('');
+		
+		return false;
 	});
 	
 	$("#itemList").delegate('.name', // The child element to bind to
                                'dblclick', // The event we want to bind
                                function() { // The handler
-            
-            $('.itemEditBox').hide();
-                                
-           //erase previously inputted name for list
+                                   
+            var prevItemName = $(this).text();
+            var nextItemName;     
+                        
+            //erase previously inputted name for list
             $(this).text('');
             
-            //target the next element which is the hidden text input box show and change its margin
-            $(this).next().toggle();
+            //target the next element whichis the hidden text input box show and change its margin
+            $(this).next().show();
+            $(this).next().find('.itemEditBox').attr('placeholder', prevItemName);
+            $(this).next().find('.itemEditBox').text('');
             $(this).next().css({'margin-left':'-30px'});
             
             //on keypress = 'enter', the previous node (class='name') becomes the input value and hide text input felement
             $(this).next().keypress(function(e){
                 if(e.which == 13){
-                    $(this).prev().text($(this).val());
-                    $(this).hide();
+                    nextItemName = $(this).find('.itemEditBox').val();
+                    
+                    if(nextItemName != '' && nextItemName != null){
+                        $(this).prev().text($(this).find('.itemEditBox').val());
+                        $(this).hide();
+                    }
+                    else{
+                        alert("Please enter shopping list item value");
+                    }
                 }
             });
             
             //when input field is not in focus, the previous node (class='name') becomes the input value and hide text input felement
-            $(this).next().blur(function(){
-                $(this).prev().text($(this).val());
-                $(this).hide();
+            $(this).next().find('.itemEditBox').blur(function(){
+                
+                nextItemName = $(this).val();
+                
+                if(nextItemName == '' || nextItemName == null){
+                    $(this).parent().prev().text(prevItemName);
+                    $(this).hide();
+                }
             });
+        });
+        
+   $('#sortButton').click(function(){
+        
+        var sortOrder = $('#sortOrder option:selected').text();
+        
+        if($('#alpha').prop('checked')){
+            SortByAlpha(sortOrder);
+        }
+        else if($('#priority').prop('checked')){
+            SortByPriority(sortOrder);
+        }
+        else if($('#timeCreated').prop('checked')){
+            SortByTime(sortOrder);
+        }
+        else{
+            alert("No criteria button was cheked");
+        }
     });
+});
 	
-	function AddListItem(){
+	function AddListItem(listName, e){
 		
 		var isPriority;
-		var itemName = $('#textBox').val();
-		var date = new Date(event.timeStamp);
-		var options = {
-    		weekday: "long", year: "numeric", month: "short",
-    		day: "numeric", hour: "2-digit", minute: "2-digit"
-		};
+		var itemName = listName;
+		var date = GetDate(e);
+        var options = {
+            weekday: "long", year: "numeric", month: "short",
+            day: "numeric", hour: "2-digit", minute: "2-digit"
+        };
 		
-		if($('#prioritySelect').is(':checked')){
-			isPriority = 'YES';
-		}
-		else{
-			isPriority = 'NO';
+		
+		if($('#prioritySelect').val() != null){
+			isPriority = $('#prioritySelect').val();
 		}
 		
 		$('#itemList').append('<ul class="items">'+
-    							    '<input type="checkbox">'+
+    							    '<li><input type="checkbox"></li>'+
     							    '<li class="name">'+ itemName + '</li>'+
-    							    '<input class="itemEditBox" style="display:none;"></input>'+
+    							    '<li style="display:none;"><input class="itemEditBox"></input></li>'+
     							    '<li class="priority">Priority: '+ isPriority + '</li>'+
     							    '<li class="dateCreated">Created: '+ date.toLocaleTimeString("en-us", options) +'</li>'+
     							    '</ul>');
 	}
 	
-	$('#sortButton').click(function(){
-		
-		var sortOrder = $('#sortOrder option:selected').text();
-		
-		if($('#alpha').prop('checked')){
-			SortByAlpha(sortOrder);
-		}
-		else if($('#priority').prop('checked')){
-			SortByPriority(sortOrder);
-		}
-		else if($('#timeCreated').prop('checked')){
-			SortByTime(sortOrder);
-		}
-		else{
-			alert("No criteria button was cheked");
-		}
-	});
+	function GetDate(e){
+	    var date = new Date(e.timeStamp);
+        
+        return date;
+	}
 	
 	function SortByAlpha(ordering){
 		
@@ -126,19 +149,6 @@ $(document).ready(function(){
 	function SortByPriority(ordering){
 		if(ordering == 'Ascending'){
 			$('#itemList ul').sort(function(a,b){  
-	    		return $(a).context.children[2].innerHTML.toLowerCase() > $(b).context.children[2].innerHTML.toLowerCase();
-			}).appendTo('#itemList');  
-		}
-		else if(ordering == 'Descending'){
-			$('#itemList ul').sort(function(a,b){  
-	    		return $(a).context.children[2].innerHTML.toLowerCase() < $(b).context.children[2].innerHTML.toLowerCase();
-			}).appendTo('#itemList'); 
-		}
-	}
-	
-	function SortByTime(ordering){
-		if(ordering == 'Ascending'){
-			$('#itemList ul').sort(function(a,b){  
 	    		return $(a).context.children[3].innerHTML.toLowerCase() > $(b).context.children[3].innerHTML.toLowerCase();
 			}).appendTo('#itemList');  
 		}
@@ -149,4 +159,16 @@ $(document).ready(function(){
 		}
 	}
 	
-});
+	function SortByTime(ordering){
+		if(ordering == 'Ascending'){
+			$('#itemList ul').sort(function(a,b){  
+	    		return $(a).context.children[4].innerHTML.toLowerCase() > $(b).context.children[4].innerHTML.toLowerCase();
+			}).appendTo('#itemList');  
+		}
+		else if(ordering == 'Descending'){
+			$('#itemList ul').sort(function(a,b){  
+	    		return $(a).context.children[4].innerHTML.toLowerCase() < $(b).context.children[4].innerHTML.toLowerCase();
+			}).appendTo('#itemList'); 
+		}
+	}
+	
